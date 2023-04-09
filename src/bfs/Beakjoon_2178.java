d396250b28a77697042609fd598217a9bfd7361b
gns5780 <gns5780@naver.com>
2023-04-09 19:48:41 +0900

    BFS 학습

---
+++ src/bfs/Beakjoon_2178.java
@@ -0,0 +1,57 @@
+package bfs;
+
+import java.util.Scanner;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Beakjoon_2178 {
+	
+	static int[] dx = {0,1,0,-1};
+	static int[] dy = {1,0,-1,0};
+
+	static boolean[][] visited;
+	static int[][] arr;
+	static int n;
+	static int m;
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		n = sc.nextInt();
+		m = sc.nextInt();
+		arr = new int[n][m];
+		visited = new boolean[n][m];
+		
+		for(int i=0;i<n;i++) {
+			String request = sc.next();
+			for(int j=0;j<m;j++) {
+				arr[i][j] = Integer.parseInt(request.substring(j,j+1));
+			}
+		}
+		BFS(0,0);
+		
+		System.out.println(arr[n-1][m-1]);
+	}
+	public static void BFS(int i, int j) {
+		Queue<int[]> queue = new LinkedList<>();
+		queue.offer(new int[] {i,j});
+		while(!queue.isEmpty()) {
+			int now[] = queue.poll();
+			visited[i][j] = true;
+			for(int k=0; k<4;k++) {
+				int x = now[0] + dx[k];
+				int y = now[1] + dy[k];
+				if(x >= 0 && y>= 0 && x < n && y < m) {
+					if(arr[x][y] != 0 && visited[x][y] == false) {
+						visited[x][y] = true;
+						arr[x][y] = arr[now[0]][now[1]] + 1;
+						queue.add(new int[] {x,y});
+						
+					}
+				}
+				
+			}
+		}
+	}
+
+}
---
+++ src/bfs/Beakjoon_2178_re_miwansung.java
@@ -0,0 +1,57 @@
+package bfs;
+
+import java.util.Scanner;
+import java.util.LinkedList;
+import java.util.Queue;
+public class Beakjoon_2178_re_miwansung {
+	
+	static int n;
+	static int m;
+	static boolean[][] visited;
+	static int[][] arr;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		n = sc.nextInt();
+		m = sc.nextInt();
+		
+		visited = new boolean[n][m];
+		arr = new int[n][m];
+		
+		for(int i=0;i<n;i++) {
+			String request = sc.next();
+			for(int j=0;j<m;j++) {
+				arr[i][j] = Integer.parseInt(request.substring(j,j+1));
+			}
+		}
+		
+		BFS();
+		System.out.println(arr[n-1][m-1]);
+	}
+	public static void BFS() {
+		Queue<int[]> queue = new LinkedList<>();
+		int[] dx = {0,1,0,-1};
+		int[] dy = {1,0,-1,0};
+		int[] now = new int[2];
+		
+		queue.add(new int[] {0,0});
+		
+		while(!queue.isEmpty()) {
+			now = queue.poll();
+			for(int i=0;i<4;i++) {
+				int x = now[0]+ dx[i];
+				int y = now[1]+ dy[i];
+				
+				if(x >= 0 && y >= 0 && x < n && y < m) {
+					if(arr[x][y] != 0 && visited[x][y] != true) {
+						visited[x][y] = true;
+						queue.add(new int[] {x,y});
+						arr[x][y] = arr[now[0]][now[1]] + 1;
+					}
+				}
+			}
+		}
+		
+	}
+}
